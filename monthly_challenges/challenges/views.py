a00feef4affb0fb54b78727334ff2acf170ef7e8
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-04 12:30:28 -0500

    Proj monthly_challenges: added routes for challenges/february and challenged/january

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(req):
+  return HttpResponse("Jan")
+
+def index2(req):
+  return HttpResponse("Feb")
