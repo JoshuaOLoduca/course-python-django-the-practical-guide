69b3ca38b0aa3ac62e36d46e1f1fdc251fef5c92
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-04 14:00:01 -0500

    added index for challenges/

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,4 @@
+from ast import arg
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
@@ -18,6 +19,18 @@ challenges = {
     "november": "november",
     "december": "december",
 }
+
+
+def index(req):
+    month_list_items = ""
+
+    for month in challenges:
+        month_url = reverse("str_monthly_challenge", args=[month])
+        month_list_items += f'<li><a href="{month_url}">{month}</a></li>'
+
+    month_list = f"<ul>{month_list_items}</ul>"
+
+    return HttpResponse(month_list)
 
 
 def monthly_challenge(req, month):
