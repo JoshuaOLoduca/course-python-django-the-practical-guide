a26d2f4f102ca9b376251e019d629953a2464707
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-24 12:18:58 -0500

    adding navbar as html partial
    - Added navbar to index
    - Added navbar to challenge.html

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from challenges import views
 
 # URLConf
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month_id>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="str_monthly_challenge"),
 ]
